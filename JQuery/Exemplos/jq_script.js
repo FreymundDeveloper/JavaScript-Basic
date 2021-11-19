af3774c6cc2dc9ead851846c3aea642d2f023413
Lucas Freymund <lucasehresmann.osik@gmail.com>
2021-11-19 03:33:50 -0300

    Inútil, mas interessante

---
+++ JQuery/Exemplos/jq_script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#ocultar').click(function () { 
+        $('h1').hide();
+        
+    });
+
+    $('#cor').click(function () { 
+        $('p').css("color", "blue");
+    });
+    
+});
