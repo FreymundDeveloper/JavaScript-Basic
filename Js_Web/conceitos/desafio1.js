209293fa92fa911dcace276688880df75bea501b
Lucas Freymund <lucasehresmann.osik@gmail.com>
2022-01-13 03:12:01 -0300

    Vergonha

---
+++ Js_Web/conceitos/desafio1.js
@@ -0,0 +1,18 @@
+{
+    console.log('1')
+    console.log('3')
+    console.log('5')
+    console.log('7')
+    console.log('9')
+}
+
+{
+    console.log('2')
+    console.log('4')
+    console.log('6')
+    console.log('8')
+    console.log('10')
+}
+
+console.log('\nbloco1:\n1\n3\n5\n7\n9');
+console.log('\nbloco2:\n2\n4\n6\n8\n10');
