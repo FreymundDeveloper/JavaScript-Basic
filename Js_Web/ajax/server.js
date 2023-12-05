7793602413cda4ef779c64add5268d09f1b5043c
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-12-05 03:55:03 -0300

    Ajax -  APIs in Fetch

--- Js_Web/ajax/server.js
+++ Js_Web/ajax/server.js
@@ -6,4 +6,11 @@ app.use(express.static('.'));
 app.use(bodyParser.urlencoded( { extended: true } ));
 app.use(bodyParser.json());
 
+app.post('/form', (request, response) => {
+    response.send({
+        ...request.body,
+        id: 1
+    });
+});
+
 app.listen(8080, () => console.log('Works...'));
