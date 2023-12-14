4e9be3459b7cb5556596081e914c52a16105fe5f
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-12-13 22:41:29 -0300

    Webpack - Auto Environment Configuration

--- Js_Web/webpack/src/index.js
+++ Js_Web/webpack/src/index.js
@@ -1,4 +1,4 @@
-import Person from './person';
+import Person from './js/person';
 import './modules/module_call'
 import './assets/css/style.css'
 
--- Js_Web/webpack/webpack.config.js
+++ Js_Web/webpack/webpack.config.js
@@ -1,12 +1,24 @@
+const devMod = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const minimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: devMod ? 'development' : 'production',
     entry: './src/index.js',
     output: {
         filename: 'builder.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+            new minimizerPlugin({})
+        ],
     },
     /*plugins: [    // external model
         new MiniCssExtractPlugin({
