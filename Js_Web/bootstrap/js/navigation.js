69b496d578f3e22f4ed5d98c23bd9492cac3f705
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-12-19 00:25:17 -0300

    Bootstrap Base - Still Have Content to Delete

---
+++ Js_Web/bootstrap/js/navigation.js
@@ -0,0 +1,38 @@
+(function () {destiny
+    function browseByAjax(hash) {
+        if (!hash) return
+
+        const link = document.querySelector(`[wm-link='${hash}']`)
+        if(!link) return
+
+        const destiny = document.querySelector('[wm-link-destiny]')
+
+        const url = hash.substring(1)
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                destiny.innerHTML = html
+            })
+    }
+
+    function linksConfig() {
+        document.querySelectorAll('[wm-link]')
+            .forEach(link => {
+                link.href = link.attributes['wm-link'].value
+            })
+    }
+
+    function startedBrowse() {
+        if (location.hash) {
+            browseByAjax(location.hash)
+        } else {
+            const firstLink = document.querySelector('[wm-link]')
+            browseByAjax(firstLink.hash)
+        }
+    }
+
+    window.onhashchange = e => browseByAjax(location.hash)
+    
+    linksConfig()
+    startedBrowse()
+})()
