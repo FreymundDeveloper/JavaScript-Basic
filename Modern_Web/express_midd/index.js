3d4614a6d607dfe1998a48b1f4da853002bf3670
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-02-23 17:13:55 -0300

    Express Midd - Variation of Data in The Body

--- Modern_Web/express_midd/index.js
+++ Modern_Web/express_midd/index.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const callSomething = require('./mid');
 
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(callSomething('Midd Call'));
 
+// Parms Content
 app.get('/clients/return', (req, res) => {
-    res.send(`Query is ${re.query.test}`);
+    res.send(`Query is ${req.query.test}`);
 });
 
 app.get('/clients/:id', (req, res) => {
     res.send(`Param is ${req.params.id}`);
 });
 
+// Body Content
+app.post('/body', (req, res) => {
+    let body = '';
+
+    req.on('data', function(part) {
+        body += part;
+    });
+
+    req.on('end', function() {
+        res.send(body);
+    });
+});
+
+app.post('/bodyparse', (req, res) => {
+    res.send(JSON.stringify(req.body));
+});
+
+// Midd and Basic Content
 app.get('/send', (req, res, next) => {
-    console.log('Next Call')
+    console.log('Next Call');
     next();
 });
 
