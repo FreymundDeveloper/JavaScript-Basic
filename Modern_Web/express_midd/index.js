64a66d9c67ae12eac80a25062ac0b72c78cee59c
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-02-23 01:59:22 -0300

    Express Midd - Launcher, Resquest and Response Types

--- Modern_Web/express_midd/index.js
+++ Modern_Web/express_midd/index.js
@@ -1,1 +1,20 @@
-// Content
+const express = require('express');
+const app = express();
+
+app.use('/json', (req, res) => {
+    res.json({
+        data: [
+            {id: 1, name: "TestOne"},
+            {id: 2, name: "TestTwo"}
+        ],
+        limit: 10
+    })
+})
+
+app.use('/send', (req, res) => {
+    res.send("works!")
+})
+
+app.listen(3001, () => {
+    console.log("Backend is running!");
+});
