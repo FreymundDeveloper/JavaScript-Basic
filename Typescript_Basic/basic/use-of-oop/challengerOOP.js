edf03af508834f6010d021168dc24db5c38329e2
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-22 03:23:35 -0300

    TS - Class Methods Manipulation

--- Typescript_Basic/basic/use-of-oop/challengerOOP.js
+++ Typescript_Basic/basic/use-of-oop/challengerOOP.js
@@ -5,11 +5,16 @@ class Product {
         this.name = name;
         this.price = price;
         this.discount = discount;
-        this.price = price - ((price / 100) * (100 * discount));
+    }
+    // Challenger Two
+    calculate() {
+        return parseFloat((this.price * (1 - this.discount)).toFixed(2));
+    }
+    resume() {
+        return `${this.name} cost R$${this.calculate()} (${this.discount * 100}% of discount)`;
     }
 }
 const game = new Product('Dark Souls', 79.99);
 const hardware = new Product('RTX', 3999, 0.5);
-console.log(game, hardware);
-// Challenger Two
+console.log(`${game.resume()} and ${hardware.resume()}`);
 // Challenger Three
--- Typescript_Basic/basic/use-of-oop/challengerOOP.ts
+++ Typescript_Basic/basic/use-of-oop/challengerOOP.ts
@@ -2,16 +2,23 @@
 
 class Product {
     constructor(public name: string, public price: number, public discount: number = 0) {
-        this.price = price - ((price / 100) * (100 * discount))
+    }
+
+    // Challenger Two
+
+    private calculate(): number {
+        return parseFloat((this.price * (1 - this.discount)).toFixed(2))
+    }
+
+    public resume(): string {
+        return `${this.name} cost R$${this.calculate()} (${this.discount * 100}% of discount)`
     }
 }
 
 const game = new Product('Dark Souls', 79.99)
 const hardware = new Product('RTX', 3999, 0.5)
 
-console.log(game, hardware)
-
-// Challenger Two
+console.log(`${game.resume()} and ${ hardware.resume()}`)
 
 
 // Challenger Three
