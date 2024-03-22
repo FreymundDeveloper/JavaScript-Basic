e9c1db8121d3120a62fff288bef46a1d33320991
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-22 17:16:29 -0300

    TS - OOP Miscellanies

--- Typescript_Basic/basic/use-of-oop/challengerOOP.js
+++ Typescript_Basic/basic/use-of-oop/challengerOOP.js
@@ -18,3 +18,55 @@ const game = new Product('Dark Souls', 79.99);
 const hardware = new Product('RTX', 3999, 0.5);
 console.log(`${game.resume()} and ${hardware.resume()}`);
 // Challenger Three
+class Motorcycle {
+    constructor(name) {
+        this.name = name;
+        this.speed = 0;
+    }
+    honk() {
+        console.log('Toooooooooot!');
+    }
+    accelerate(delta) {
+        return this.speed = this.speed + delta;
+    }
+}
+const moto = new Motorcycle('Ducati');
+moto.honk();
+console.log(moto.speed);
+moto.accelerate(30);
+console.log(moto.speed);
+// Challenger Four
+class Object2D {
+    constructor(width = 0, height = 0) {
+        this.width = width;
+        this.height = height;
+    }
+}
+class Rectangle extends Object2D {
+    area() {
+        return this.width * this.height;
+    }
+}
+const rectangle = new Rectangle(5, 7);
+console.log(rectangle.area());
+// Challenger Five
+class Internship {
+    constructor() {
+        this._firstName = '';
+    }
+    static getInternship() {
+        return Internship.internship;
+    }
+    get name() {
+        return this._firstName;
+    }
+    set name(text) {
+        (text.length >= 3) ? this._firstName = text : this._firstName = '';
+    }
+}
+Internship.internship = new Internship;
+console.log(Internship.getInternship().name);
+Internship.getInternship().name = 'Le';
+console.log(Internship.getInternship().name);
+Internship.getInternship().name = 'Leonardo';
+console.log(Internship.getInternship().name);
--- Typescript_Basic/basic/use-of-oop/challengerOOP.ts
+++ Typescript_Basic/basic/use-of-oop/challengerOOP.ts
@@ -20,5 +20,72 @@ const hardware = new Product('RTX', 3999, 0.5)
 
 console.log(`${game.resume()} and ${ hardware.resume()}`)
 
-
 // Challenger Three
+
+class Motorcycle {
+    public speed: number = 0
+
+    constructor(public name: string){
+
+    }
+ 
+    public honk(): void {
+        console.log('Toooooooooot!')
+    }
+ 
+    public accelerate(delta: number): number {
+        return this.speed = this.speed + delta
+    }
+}
+ 
+const moto = new Motorcycle('Ducati')
+moto.honk()
+console.log(moto.speed)
+moto.accelerate(30)
+console.log(moto.speed)
+ 
+// Challenger Four
+
+abstract class Object2D {
+    constructor(public width: number = 0, public height: number = 0){
+
+    }
+
+    abstract area(): number
+}
+
+class Rectangle extends Object2D {
+    public area(): number {
+        return this.width * this.height
+    }
+}
+ 
+const rectangle = new Rectangle(5, 7)
+console.log(rectangle.area())
+ 
+// Challenger Five
+
+class Internship {
+    private _firstName: string = ''
+
+    private static internship: Internship = new Internship
+    private constructor(){}
+
+    static getInternship(): Internship {
+        return Internship.internship
+    }
+
+    get name(): string {
+        return this._firstName
+    }
+
+    set name(text: string) {
+        (text.length >= 3) ? this._firstName = text : this._firstName = ''
+    }
+}
+ 
+console.log(Internship.getInternship().name)
+Internship.getInternship().name = 'Le'
+console.log(Internship.getInternship().name)
+Internship.getInternship().name = 'Leonardo'
+console.log(Internship.getInternship().name)
