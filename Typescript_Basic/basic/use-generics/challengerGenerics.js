12e63f7c1bff94609bb3b147490541e9d0a71125
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-03-26 04:30:30 -0300

    TS - Generics on Everything

--- Typescript_Basic/basic/use-generics/challengerGenerics.js
+++ Typescript_Basic/basic/use-generics/challengerGenerics.js
@@ -1,4 +1,5 @@
 "use strict";
+// Challenger One
 class Line {
     constructor(line = []) {
         this.line = line;
@@ -20,3 +21,33 @@ line.enter('Elena');
 console.log(line.printLine());
 console.log(line.nextInLine());
 console.log(line.printLine());
+class Mapping {
+    constructor() {
+        this.arraylist = new Array();
+    }
+    justPush(obj) {
+        const equal = this.take(obj.key);
+        equal !== null ? equal.value = obj.value : this.arraylist.push(obj);
+    }
+    take(key) {
+        const found = this.arraylist.find(item => item.key === key);
+        return found ? found : null;
+    }
+    clear() {
+        this.arraylist = new Array();
+    }
+    printMap() {
+        this.arraylist.forEach(itens => {
+            console.log(itens);
+        });
+    }
+}
+const mapping = new Mapping();
+mapping.justPush({ key: 1, value: 'Pedro' });
+mapping.justPush({ key: 2, value: 'Rebeca' });
+mapping.justPush({ key: 3, value: 'Maria' });
+mapping.justPush({ key: 1, value: 'Gustavo' });
+console.log(mapping.take(2));
+mapping.printMap();
+mapping.clear();
+mapping.printMap();
--- Typescript_Basic/basic/use-generics/challengerGenerics.ts
+++ Typescript_Basic/basic/use-generics/challengerGenerics.ts
@@ -1,3 +1,5 @@
+// Challenger One
+
 class Line<T> {
     constructor(private line: Array<T> = []){}
 
@@ -21,3 +23,45 @@ line.enter('Elena')
 console.log(line.printLine())
 console.log(line.nextInLine())
 console.log(line.printLine())
+
+// Challenger Two
+
+type ArrayMapping<N, S> = {key: N, value: S}
+
+class Mapping<N extends number, S> {
+    private arraylist: Array<ArrayMapping<N, S>> = new Array<ArrayMapping<N, S>>()
+
+    constructor(){}
+
+    public justPush(obj: ArrayMapping<N, S>): void {
+        const equal = this.take(obj.key)
+        equal !== null ? equal.value = obj.value : this.arraylist.push(obj)
+    }
+
+    public take(key: N): ArrayMapping<N, S> | null {
+        const found = this.arraylist.find(item => item.key === key)
+        return found ? found : null
+    }
+
+    public clear(): void {
+        this.arraylist = new Array<ArrayMapping<N, S>>()
+    }
+
+    public printMap(): void {
+        this.arraylist.forEach(itens => {
+            console.log(itens)
+        })
+    }
+
+}
+
+const mapping = new Mapping<number, string>()
+mapping.justPush({ key: 1, value: 'Pedro' })
+mapping.justPush({ key: 2, value: 'Rebeca' })
+mapping.justPush({ key: 3, value: 'Maria' })
+mapping.justPush({ key: 1, value: 'Gustavo' })
+ 
+console.log(mapping.take(2))
+mapping.printMap()
+mapping.clear()
+mapping.printMap()
